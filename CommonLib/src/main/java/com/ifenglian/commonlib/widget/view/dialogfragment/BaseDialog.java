427d9b11a8b279b3ae8fbd8cd9df928970fa1397
xiaofy1 <xiaofy@ifenglian.com>
2017-12-02 13:14:06 +0800

    update

--- CommonLib/src/main/java/com/ifenglian/commonlib/widget/view/dialogfragment/BaseDialog.java
+++ CommonLib/src/main/java/com/ifenglian/commonlib/widget/view/dialogfragment/BaseDialog.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.ifenglian.commonlib.R;
 
