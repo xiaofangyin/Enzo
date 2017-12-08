c82ee654ead2f9a16ebf75f468ea81dba4cd693e
xiaofy1 <xiaofy@ifenglian.com>
2017-12-08 13:59:56 +0800

    版本更新

--- Enzo/app/src/main/java/com/ifenglian/enzo/App.java
+++ Enzo/app/src/main/java/com/ifenglian/enzo/App.java
@@ -7,6 +7,10 @@ import com.ifenglian.module_a.plugin.MAFactoryManager;
 import com.ifenglian.module_b.plugin.MBFactoryManager;
 import com.ifenglian.module_c.plugin.MCFactoryManager;
 import com.ifenglian.module_d.plugin.MDFactoryManager;
+import com.lzy.okhttputils.OkHttpUtils;
+import com.lzy.okhttputils.cookie.store.PersistentCookieStore;
+import com.lzy.okhttputils.model.HttpHeaders;
+import com.lzy.okhttputils.model.HttpParams;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +26,30 @@ public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        initOkHttp();
         initFactory();
+    }
+
+    private void initOkHttp() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("commonHeaderKey1", "commonHeaderValue1");    //所有的 header 都 不支持 中文
+        headers.put("commonHeaderKey2", "commonHeaderValue2");
+        HttpParams params = new HttpParams();
+        params.put("commonParamsKey1", "commonParamsValue1");     //所有的 params 都 支持 中文
+        params.put("commonParamsKey2", "这里支持中文参数");
+
+        //必须调用初始化
+        OkHttpUtils.init(this);
+        //以下都不是必须的，根据需要自行选择
+        OkHttpUtils.getInstance()//
+                .debug("OkHttpUtils")                                              //是否打开调试
+                .setConnectTimeout(OkHttpUtils.DEFAULT_MILLISECONDS)               //全局的连接超时时间
+                .setReadTimeOut(OkHttpUtils.DEFAULT_MILLISECONDS)                  //全局的读取超时时间
+                .setWriteTimeOut(OkHttpUtils.DEFAULT_MILLISECONDS)                 //全局的写入超时时间
+//                .setCookieStore(new MemoryCookieStore())                           //cookie使用内存缓存（app退出后，cookie消失）
+                .setCookieStore(new PersistentCookieStore())                       //cookie持久化存储，如果cookie不过期，则一直有效
+                .addCommonHeaders(headers)                                         //设置全局公共头
+                .addCommonParams(params);                                          //设置全局公共参数
     }
 
     private void initFactory() {
@@ -31,6 +58,6 @@ public class App extends BaseApplication {
         factoryList.add(MBFactoryManager.getInstance());
         factoryList.add(MCFactoryManager.getInstance());
         factoryList.add(MDFactoryManager.getInstance());
-        SAFactoryManager.getInstance().init(App.this,factoryList);
+        SAFactoryManager.getInstance().init(App.this, factoryList);
     }
 }
