b18b4bba68317395835cbc098b8cd4b4dac13562
xiaofy1 <xiaofy@ifenglian.com>
2017-12-08 21:38:32 +0800

    版本更新

--- CommonLib/src/main/java/com/ifenglian/commonlib/utils/updateversion/UpdateVersionService.java
+++ CommonLib/src/main/java/com/ifenglian/commonlib/utils/updateversion/UpdateVersionService.java
@@ -9,11 +9,8 @@ import android.os.IBinder;
 import android.widget.RemoteViews;
 
 import com.ifenglian.commonlib.R;
-import com.ifenglian.commonlib.utils.common.ApkUtils;
 import com.ifenglian.commonlib.utils.common.PreferenceUtils;
 import com.ifenglian.commonlib.utils.common.SDCardUtils;
-import com.tsy.sdk.myokhttp.MyOkHttp;
-import com.tsy.sdk.myokhttp.response.DownloadResponseHandler;
 
 import java.io.File;
 
@@ -60,46 +57,46 @@ public class UpdateVersionService extends Service {
     }
 
     public void downLoadFile(String url) {
-        MyOkHttp.get().download(url, SDCardUtils.getRootDirectory() + "/updateVersion", UpdateVersionUtil.DOWN_LOAD_APP_NAME, new DownloadResponseHandler() {
-            @Override
-            public void onFinish(File download_file) {
-                // 更改文字
-                notification.contentView.setTextViewText(R.id.msg, "下载完成!点击安装");
-                // 发送消息
-                nm.notify(0, notification);
-                stopSelf();
-                //收起通知栏
-                UpdateVersionUtil.collapseStatusBar(UpdateVersionService.this);
-                //自动安装新版本
-                Intent installIntent = ApkUtils.getInstallIntent(updateFile);
-                startActivity(installIntent);
-            }
-
-            @Override
-            public void onProgress(long currentBytes, long totalBytes) {
-                if (initTotal == 0) {//说明第一次开始下载
-                    initTotal = totalBytes;
-                }
-                if (initTotal != totalBytes) {//说明下载过程中暂停过，文件的总长度出现问题  就把初始的文件的长度赋值给他重新计算已经下载的比例
-                    totalBytes = initTotal;
-                }
-                long l = currentBytes * 100 / totalBytes;
-                notification.contentView.setTextViewText(R.id.msg, "正在下载：一起去吃鸡");
-                // 更改文字
-                notification.contentView.setTextViewText(R.id.bartext, l + "%");
-                // 更改进度条
-                notification.contentView.setProgressBar(R.id.progressBar1, 100, (int) l, false);
-                // 发送消息
-                nm.notify(0, notification);
-            }
-
-            @Override
-            public void onFailure(String error_msg) {
-                notification.contentView.setTextViewText(R.id.msg, "网络异常！请检查网络设置！");
-                // 发送消息
-                nm.notify(0, notification);
-            }
-        });
+//        MyOkHttp.get().download(url, SDCardUtils.getRootDirectory() + "/updateVersion", UpdateVersionUtil.DOWN_LOAD_APP_NAME, new DownloadResponseHandler() {
+//            @Override
+//            public void onFinish(File download_file) {
+//                // 更改文字
+//                notification.contentView.setTextViewText(R.id.msg, "下载完成!点击安装");
+//                // 发送消息
+//                nm.notify(0, notification);
+//                stopSelf();
+//                //收起通知栏
+//                UpdateVersionUtil.collapseStatusBar(UpdateVersionService.this);
+//                //自动安装新版本
+//                Intent installIntent = ApkUtils.getInstallIntent(updateFile);
+//                startActivity(installIntent);
+//            }
+//
+//            @Override
+//            public void onProgress(long currentBytes, long totalBytes) {
+//                if (initTotal == 0) {//说明第一次开始下载
+//                    initTotal = totalBytes;
+//                }
+//                if (initTotal != totalBytes) {//说明下载过程中暂停过，文件的总长度出现问题  就把初始的文件的长度赋值给他重新计算已经下载的比例
+//                    totalBytes = initTotal;
+//                }
+//                long l = currentBytes * 100 / totalBytes;
+//                notification.contentView.setTextViewText(R.id.msg, "正在下载：一起去吃鸡");
+//                // 更改文字
+//                notification.contentView.setTextViewText(R.id.bartext, l + "%");
+//                // 更改进度条
+//                notification.contentView.setProgressBar(R.id.progressBar1, 100, (int) l, false);
+//                // 发送消息
+//                nm.notify(0, notification);
+//            }
+//
+//            @Override
+//            public void onFailure(String error_msg) {
+//                notification.contentView.setTextViewText(R.id.msg, "网络异常！请检查网络设置！");
+//                // 发送消息
+//                nm.notify(0, notification);
+//            }
+//        });
     }
 
     @Override
