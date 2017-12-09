5df8570f7d169cf413f8a3a7b4baaba2e8d1dbe4
xiaofy1 <xiaofy@ifenglian.com>
2017-12-09 14:19:38 +0800

    update

--- CommonLib/src/main/java/com/ifenglian/commonlib/utils/updateversion/UpdateVersionService.java
+++ CommonLib/src/main/java/com/ifenglian/commonlib/utils/updateversion/UpdateVersionService.java
@@ -6,13 +6,23 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.ifenglian.commonlib.BaseCallBack;
 import com.ifenglian.commonlib.R;
+import com.ifenglian.commonlib.utils.OkHttpManager;
+import com.ifenglian.commonlib.utils.common.ApkUtils;
 import com.ifenglian.commonlib.utils.common.PreferenceUtils;
 import com.ifenglian.commonlib.utils.common.SDCardUtils;
+import com.ifenglian.commonlib.utils.toast.ToastUtils;
 
 import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author wenjie
@@ -33,7 +43,7 @@ public class UpdateVersionService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        updateFile = new File(SDCardUtils.getRootDirectory() + "/updateVersion",UpdateVersionUtil.DOWN_LOAD_APP_NAME);
+        updateFile = new File(SDCardUtils.getRootDirectory() + "/updateVersion", UpdateVersionUtil.DOWN_LOAD_APP_NAME);
 
         nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notification = new Notification();
@@ -57,46 +67,63 @@ public class UpdateVersionService extends Service {
     }
 
     public void downLoadFile(String url) {
-//        MyOkHttp.get().download(url, SDCardUtils.getRootDirectory() + "/updateVersion", UpdateVersionUtil.DOWN_LOAD_APP_NAME, new DownloadResponseHandler() {
-//            @Override
-//            public void onFinish(File download_file) {
-//                // 更改文字
-//                notification.contentView.setTextViewText(R.id.msg, "下载完成!点击安装");
-//                // 发送消息
-//                nm.notify(0, notification);
-//                stopSelf();
-//                //收起通知栏
-//                UpdateVersionUtil.collapseStatusBar(UpdateVersionService.this);
-//                //自动安装新版本
-//                Intent installIntent = ApkUtils.getInstallIntent(updateFile);
-//                startActivity(installIntent);
-//            }
-//
-//            @Override
-//            public void onProgress(long currentBytes, long totalBytes) {
-//                if (initTotal == 0) {//说明第一次开始下载
-//                    initTotal = totalBytes;
-//                }
-//                if (initTotal != totalBytes) {//说明下载过程中暂停过，文件的总长度出现问题  就把初始的文件的长度赋值给他重新计算已经下载的比例
-//                    totalBytes = initTotal;
-//                }
-//                long l = currentBytes * 100 / totalBytes;
-//                notification.contentView.setTextViewText(R.id.msg, "正在下载：一起去吃鸡");
-//                // 更改文字
-//                notification.contentView.setTextViewText(R.id.bartext, l + "%");
-//                // 更改进度条
-//                notification.contentView.setProgressBar(R.id.progressBar1, 100, (int) l, false);
-//                // 发送消息
-//                nm.notify(0, notification);
-//            }
-//
-//            @Override
-//            public void onFailure(String error_msg) {
-//                notification.contentView.setTextViewText(R.id.msg, "网络异常！请检查网络设置！");
-//                // 发送消息
-//                nm.notify(0, notification);
-//            }
-//        });
+        OkHttpManager.getInstance().asynDownloadFile(url,
+                SDCardUtils.getRootDirectory() + "/updateVersion", UpdateVersionUtil.DOWN_LOAD_APP_NAME,
+                new BaseCallBack<String>() {
+                    @Override
+                    public void onRequestBefore(Request request) {
+                        Log.e("AAA", "onRequestBefore...");
+                        ToastUtils.showShortToast("开始下载");
+                    }
+
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.e("AAA", "onFailure...");
+                        notification.contentView.setTextViewText(R.id.msg, "网络异常！请检查网络设置！");
+                        // 发送消息
+                        nm.notify(0, notification);
+                    }
+
+                    @Override
+                    public void onSuccess(Call call, Response response, String o) {
+                        Log.e("AAA", "onSuccess...");
+                        // 更改文字
+                        notification.contentView.setTextViewText(R.id.msg, "下载完成!点击安装");
+                        // 发送消息
+                        nm.notify(0, notification);
+                        stopSelf();
+                        //收起通知栏
+                        UpdateVersionUtil.collapseStatusBar(UpdateVersionService.this);
+                        //自动安装新版本
+                        Intent installIntent = ApkUtils.getInstallIntent(updateFile);
+                        startActivity(installIntent);
+                    }
+
+                    @Override
+                    public void onResponse(Response response) {
+                        Log.e("AAA", "onResponse...");
+                    }
+
+                    @Override
+                    public void onError(Call call, int statusCode, Exception e) {
+                        Log.e("AAA", "onError...");
+                        notification.contentView.setTextViewText(R.id.msg, "网络异常！请检查网络设置！");
+                        // 发送消息
+                        nm.notify(0, notification);
+                    }
+
+                    @Override
+                    public void inProgress(int progress, long total, int id) {
+                        Log.e("AAA", "inProgress... progress: " + progress);
+                        notification.contentView.setTextViewText(R.id.msg, "正在下载：一起去吃鸡");
+                        // 更改文字
+                        notification.contentView.setTextViewText(R.id.bartext, progress + "%");
+                        // 更改进度条
+                        notification.contentView.setProgressBar(R.id.progressBar1, 100, progress, false);
+                        // 发送消息
+                        nm.notify(0, notification);
+                    }
+                });
     }
 
     @Override
