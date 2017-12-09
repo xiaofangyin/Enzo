04f505d0b6ade116f764c4bac6849db856d12104
xiaofy1 <xiaofy@ifenglian.com>
2017-12-09 12:58:26 +0800

    update

--- Main/src/main/java/com/ifenglian/main/activity/SAMainActivity.java
+++ Main/src/main/java/com/ifenglian/main/activity/SAMainActivity.java
@@ -1,6 +1,5 @@
 package com.ifenglian.main.activity;
 
-import android.Manifest;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
