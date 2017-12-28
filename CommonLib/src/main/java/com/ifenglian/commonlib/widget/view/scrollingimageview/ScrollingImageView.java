ac0e2857f228b251d52589a486f1f070ccc5236c
xiaofy1 <xiaofy@ifenglian.com>
2017-12-28 11:06:34 +0800

    scrolling view

--- CommonLib/src/main/java/com/ifenglian/commonlib/widget/view/scrollingimageview/ScrollingImageView.java
+++ CommonLib/src/main/java/com/ifenglian/commonlib/widget/view/scrollingimageview/ScrollingImageView.java
@@ -7,15 +7,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
 
 import com.ifenglian.commonlib.R;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import static java.lang.Math.abs;
 import static java.util.Collections.singletonList;
@@ -40,68 +37,21 @@ public class ScrollingImageView extends View {
 
     private void init(Context context, AttributeSet attrs) {
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ScrollingImageView, 0, 0);
-        int initialState;
         try {
-            initialState = ta.getInt(R.styleable.ScrollingImageView_initialState, 0);
             speed = ta.getDimension(R.styleable.ScrollingImageView_speed, 10);
-            int sceneLength = ta.getInt(R.styleable.ScrollingImageView_sceneLength, 1000);
-            final int randomnessResourceId = ta.getResourceId(R.styleable.ScrollingImageView_randomness, 0);
-            int[] randomness = new int[0];
-            if (randomnessResourceId > 0) {
-                randomness = getResources().getIntArray(randomnessResourceId);
-            }
-
-            int type = isInEditMode() ? TypedValue.TYPE_STRING : ta.peekValue(R.styleable.ScrollingImageView_src).type;
-            if (type == TypedValue.TYPE_REFERENCE) {
-                int resourceId = ta.getResourceId(R.styleable.ScrollingImageView_src, 0);
-                TypedArray typedArray = getResources().obtainTypedArray(resourceId);
-                try {
-                    int bitmapsSize = 0;
-                    for (int r : randomness) {
-                        bitmapsSize += r;
-                    }
-
-                    bitmaps = new ArrayList<>(Math.max(typedArray.length(), bitmapsSize));
-
-                    for (int i = 0; i < typedArray.length(); i++) {
-                        int multiplier = 1;
-                        if (randomness.length > 0 && i < randomness.length) {
-                            multiplier = Math.max(1, randomness[i]);
-                        }
-
-                        Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), typedArray.getResourceId(i, 0));
-                        for (int m = 0; m < multiplier; m++) {
-                            bitmaps.add(bitmap);
-                        }
-
-                        maxBitmapHeight = Math.max(bitmap.getHeight(), maxBitmapHeight);
-                    }
-
-                    Random random = new Random();
-                    this.scene = new int[sceneLength];
-                    for (int i = 0; i < this.scene.length; i++) {
-                        this.scene[i] = random.nextInt(bitmaps.size());
-                    }
-                } finally {
-                    typedArray.recycle();
-                }
-            } else if (type == TypedValue.TYPE_STRING) {
-                Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), ta.getResourceId(R.styleable.ScrollingImageView_src, 0));
-                if (bitmap != null) {
-                    bitmaps = singletonList(bitmap);
-                    scene = new int[]{0};
-                    maxBitmapHeight = bitmaps.get(0).getHeight();
-                } else {
-                    bitmaps = Collections.emptyList();
-                }
+            Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), ta.getResourceId(R.styleable.ScrollingImageView_src, 0));
+            if (bitmap != null) {
+                bitmaps = singletonList(bitmap);
+                scene = new int[]{0};
+                maxBitmapHeight = bitmaps.get(0).getHeight();
+            } else {
+                bitmaps = Collections.emptyList();
             }
         } finally {
             ta.recycle();
         }
 
-        if (initialState == 0) {
-            start();
-        }
+        start();
     }
 
     @Override
