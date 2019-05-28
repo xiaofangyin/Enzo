d4d2960bd4ec772be24eb6a0af3472b4e99d1091
xiaofy1 <xiaofy@ifenglian.com>
2019-05-28 23:16:03 +0800

    主题色替换为黑色

--- CommonLib/src/main/java/com/enzo/commonlib/widget/tablayout/TabEntityConfig.java
+++ CommonLib/src/main/java/com/enzo/commonlib/widget/tablayout/TabEntityConfig.java
@@ -16,28 +16,28 @@ public class TabEntityConfig {
     public static List<TabEntity> getEntities() {
         List<TabEntity> list = new ArrayList<>();
         TabEntity entity1 = new TabEntity();
-        entity1.setTitle("政府曝光");
+        entity1.setTitle("首页");
         entity1.setNormalColor(0xFFAAAAAA);
         entity1.setSelectedColor(0xFF04d1aa);
         entity1.setNormalImage(R.mipmap.icon_home_tab_1_normal);
         entity1.setSelectedImage(R.mipmap.icon_home_tab_1_selected);
 
         TabEntity entity2 = new TabEntity();
-        entity2.setTitle("活动");
+        entity2.setTitle("关注");
         entity2.setNormalColor(0xFFAAAAAA);
         entity2.setSelectedColor(0xFF04d1aa);
         entity2.setNormalImage(R.mipmap.icon_home_tab_2_normal);
         entity2.setSelectedImage(R.mipmap.icon_home_tab_2_selected);
 
         TabEntity entity3 = new TabEntity();
-        entity3.setTitle("食品安全");
+        entity3.setTitle("消息");
         entity3.setNormalColor(0xFFAAAAAA);
         entity3.setSelectedColor(0xFF04d1aa);
         entity3.setNormalImage(R.mipmap.icon_home_tab_4_normal);
         entity3.setSelectedImage(R.mipmap.icon_home_tab_4_selected);
 
         TabEntity entity4 = new TabEntity();
-        entity4.setTitle("问答广场");
+        entity4.setTitle("我");
         entity4.setNormalColor(0xFFAAAAAA);
         entity4.setSelectedColor(0xFF04d1aa);
         entity4.setNormalImage(R.mipmap.icon_home_tab_5_normal);
