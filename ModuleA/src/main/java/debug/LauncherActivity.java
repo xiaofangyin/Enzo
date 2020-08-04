2dfc486cff46f879ef1248d6ea644f350686fb35
xiaofy <fangyin.xiao@igg.com>
2020-08-04 16:05:00 +0800

    添加组件化架构设计

---
+++ ModuleA/src/main/java/debug/LauncherActivity.java
@@ -0,0 +1,42 @@
+package debug;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.enzo.commonlib.base.BaseActivity;
+import com.enzo.module_a.R;
+import com.enzo.module_a.ui.fragment.MAFragment;
+
+public class LauncherActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.ma_activity_launcher;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+        replaceFragment();
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    private void replaceFragment() {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_content,new MAFragment());
+        transaction.commitAllowingStateLoss();
+        fragmentManager.executePendingTransactions();
+    }
+
+}
---
+++ ModuleD/src/main/java/debug/ModuleApplication.java
@@ -0,0 +1,20 @@
+package debug;
+
+import android.app.Application;
+
+public class ModuleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        login();
+    }
+
+    /**
+     * 在这里模拟登陆，然后拿到sessionId或者Token
+     * 这样就能够在组件请求接口了
+     */
+    private void login() {
+
+    }
+}
