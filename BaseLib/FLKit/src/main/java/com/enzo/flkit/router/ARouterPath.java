4ffcd4fce661a8fc2a55ad1cf0c467b8840ba807
xiaofy <xiaofangyinwork@163.com>
2020-08-05 22:35:01 +0800

    pullrefreshrecyclerview

--- BaseLib/FLKit/src/main/java/com/enzo/flkit/router/ARouterPath.java
+++ BaseLib/FLKit/src/main/java/com/enzo/flkit/router/ARouterPath.java
@@ -7,19 +7,19 @@ public class ARouterPath {
     public static final String MAIN_ADD_DEVICE = MODULE_GROUP_MAIN + "/add_device";
 
     //ModuleA
-    private static final String MODULE_GROUP_MODULE_A = "/module_a";
-    public static final String MODULE_A_TEST = MODULE_GROUP_MODULE_A + "/test";
+    private static final String MODULE_GROUP_A = "/module_a";
+    public static final String MODULE_A_TEST = MODULE_GROUP_A + "/test";
 
     //ModuleB
-    private static final String MODULE_GROUP_MODULE_B = "/module_b";
-    public static final String MODULE_B_TEST = MODULE_GROUP_MODULE_B + "/test";
+    private static final String MODULE_GROUP_B = "/module_b";
+    public static final String MODULE_B_TEST = MODULE_GROUP_B + "/test";
 
     //ModuleC
-    private static final String MODULE_GROUP_MODULE_C = "/module_c";
-    public static final String MODULE_C_TEST = MODULE_GROUP_MODULE_C + "/test";
+    private static final String MODULE_GROUP_C = "/module_c";
+    public static final String MODULE_C_TEST = MODULE_GROUP_C + "/test";
 
     //ModuleD
-    private static final String MODULE_GROUP_MODULE_D = "/module_d";
-    public static final String MODULE_D_TEST = MODULE_GROUP_MODULE_D + "/test";
+    private static final String MODULE_GROUP_D = "/module_d";
+    public static final String MODULE_D_TEST = MODULE_GROUP_D + "/test";
 
 }
