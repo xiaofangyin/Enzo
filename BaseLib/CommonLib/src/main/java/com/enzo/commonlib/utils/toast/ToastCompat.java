975414f002f8e4cdde3915f30349cac0e920566b
xiaofy <xiaofangyinwork@163.com>
2020-08-06 21:47:34 +0800

    shadow

--- BaseLib/CommonLib/src/main/java/com/enzo/commonlib/utils/toast/ToastCompat.java
+++ BaseLib/CommonLib/src/main/java/com/enzo/commonlib/utils/toast/ToastCompat.java
@@ -2,13 +2,20 @@ package com.enzo.commonlib.utils.toast;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
+
 import com.enzo.commonlib.R;
+import com.enzo.commonlib.utils.common.DensityUtil;
+import com.enzo.commonlib.utils.common.shadow.ShadowConfig;
+import com.enzo.commonlib.utils.common.shadow.ShadowHelper;
 
 public class ToastCompat {
 
@@ -18,15 +25,23 @@ public class ToastCompat {
         if (Build.VERSION.SDK_INT == 25) {
             SafeToastContext safeToastContext = new SafeToastContext(context);
             toast = Toast.makeText(safeToastContext, text, duration);
-        }else{
+        } else {
             toast = Toast.makeText(context, text, duration);
         }
-        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if(inflater != null) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (inflater != null) {
             //自定义布局
             View view = inflater.inflate(R.layout.lib_layout_toast, null);
+
+            ShadowConfig.Builder config = new ShadowConfig.Builder()
+                    .setColor(ContextCompat.getColor(context, R.color.color_orange))
+                    .setShadowColor(ContextCompat.getColor(context, R.color.color_orange))
+                    .setOffsetX(DensityUtil.dip2px(context, 0.4f))
+                    .setOffsetY(DensityUtil.dip2px(context, 0.4f));
+            ShadowHelper.setShadowBgForView(view, config);
+
             //自定义toast文本
-            TextView toastTv = (TextView) view.findViewById(R.id.tv_content_default);
+            TextView toastTv = view.findViewById(R.id.tv_content_default);
             toastTv.setText(text);
             toast.setView(view);
         }
