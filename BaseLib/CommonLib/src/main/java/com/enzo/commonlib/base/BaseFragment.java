f920e6468d0e0abbe8a7bc401fe0c6e5d3426253
xiaofy <xiaofangyinwork@163.com>
2020-08-11 21:35:43 +0800

    修复fragment重叠问题

--- BaseLib/CommonLib/src/main/java/com/enzo/commonlib/base/BaseFragment.java
+++ BaseLib/CommonLib/src/main/java/com/enzo/commonlib/base/BaseFragment.java
@@ -1,11 +1,14 @@
 package com.enzo.commonlib.base;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +19,10 @@ import org.jetbrains.annotations.NotNull;
  * 邮   箱: xiaofangyinwork@163.com
  */
 public abstract class BaseFragment extends Fragment implements IBaseFragment {
+
+    //发生Fragment重叠的根本原因在于FragmentState没有保存Fragment的显示状态，
+    //即mHidden，导致页面重启后，该值为默认的false，即show状态，所以导致了Fragment的重叠。
+    private static final String STATE_SAVE_IS_HIDDEN = "STATE_SAVE_IS_HIDDEN";
 
     @Nullable
     @Override
@@ -33,12 +40,25 @@ public abstract class BaseFragment extends Fragment implements IBaseFragment {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            boolean isSupportHidden = savedInstanceState.getBoolean(STATE_SAVE_IS_HIDDEN);
+            if (getFragmentManager() != null) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                if (isSupportHidden) {
+                    ft.hide(this);
+                } else {
+                    ft.show(this);
+                }
+                ft.commit();
+            }
+        }
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_SAVE_IS_HIDDEN, isHidden());
     }
 }
