18db0486f10939499d6a50757aa52c96406d1dc3
xiaofy <xiaofangyinwork@163.com>
2020-08-13 07:22:49 +0800

    update

--- ModuleD/src/main/java/com/enzo/module_d/ui/fragment/MDViewPagerFragment3.java
+++ ModuleD/src/main/java/com/enzo/module_d/ui/fragment/MDViewPagerFragment3.java
@@ -5,6 +5,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.enzo.commonlib.base.BaseFragment;
+import com.enzo.commonlib.widget.scrollingimageview.ScrollingImageView;
 import com.enzo.commonlib.widget.waveview.WaveView;
 import com.enzo.module_d.R;
 
@@ -16,6 +17,16 @@ import com.enzo.module_d.R;
  */
 public class MDViewPagerFragment3 extends BaseFragment {
 
+    private ScrollingImageView scrollingImageView1;
+    private ScrollingImageView scrollingImageView2;
+    private ScrollingImageView scrollingImageView3;
+    private ScrollingImageView scrollingImageView4;
+    private ScrollingImageView scrollingImageView5;
+    private ScrollingImageView scrollingImageView6;
+    private ScrollingImageView scrollingImageView7;
+    private ScrollingImageView scrollingImageView8;
+    private ScrollingImageView scrollingImageView9;
+
     @Override
     public int getLayoutId() {
         return R.layout.md_fragment_d3;
@@ -23,6 +34,15 @@ public class MDViewPagerFragment3 extends BaseFragment {
 
     @Override
     public void initView(View rootView) {
+        scrollingImageView1 = rootView.findViewById(R.id.scroll_image_view_1);
+        scrollingImageView2 = rootView.findViewById(R.id.scroll_image_view_2);
+        scrollingImageView3 = rootView.findViewById(R.id.scroll_image_view_3);
+        scrollingImageView4 = rootView.findViewById(R.id.scroll_image_view_4);
+        scrollingImageView5 = rootView.findViewById(R.id.scroll_image_view_5);
+        scrollingImageView6 = rootView.findViewById(R.id.scroll_image_view_6);
+        scrollingImageView7 = rootView.findViewById(R.id.scroll_image_view_7);
+        scrollingImageView8 = rootView.findViewById(R.id.scroll_image_view_8);
+        scrollingImageView9 = rootView.findViewById(R.id.scroll_image_view_9);
         final ImageView ivAvatar = rootView.findViewById(R.id.iv_avatar);
         WaveView waveView = rootView.findViewById(R.id.wave_view);
         final FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) ivAvatar.getLayoutParams();
@@ -46,4 +66,31 @@ public class MDViewPagerFragment3 extends BaseFragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        scrollingImageView1.start();
+        scrollingImageView2.start();
+        scrollingImageView3.start();
+        scrollingImageView4.start();
+        scrollingImageView5.start();
+        scrollingImageView6.start();
+        scrollingImageView7.start();
+        scrollingImageView8.start();
+        scrollingImageView9.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scrollingImageView1.stop();
+        scrollingImageView2.stop();
+        scrollingImageView3.stop();
+        scrollingImageView4.stop();
+        scrollingImageView5.stop();
+        scrollingImageView6.stop();
+        scrollingImageView7.stop();
+        scrollingImageView8.stop();
+        scrollingImageView9.stop();
+    }
 }
