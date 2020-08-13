60eec177966cafc651564849eaa3537048a64858
xiaofy <xiaofangyinwork@163.com>
2020-08-14 00:02:09 +0800

    update home page

--- ModuleA/src/main/java/com/enzo/module_a/ui/fragment/MAHomeSubFragment2.java
+++ ModuleA/src/main/java/com/enzo/module_a/ui/fragment/MAHomeSubFragment2.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.enzo.commonlib.base.BaseFragment;
 import com.enzo.commonlib.net.okhttp.BaseExecutor;
+import com.enzo.commonlib.utils.common.LogUtil;
 import com.enzo.commonlib.widget.pulltorefresh.recyclerview.PullToRefreshRecyclerView;
 import com.enzo.module_a.R;
 import com.enzo.module_a.model.bean.MAHomeBannerBean;
@@ -17,7 +18,10 @@ import com.enzo.module_a.model.exetutor.MAPhotoListExecutor;
 import com.enzo.module_a.ui.adapter.MAHomeAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class MAHomeSubFragment2 extends BaseFragment {
 
@@ -54,16 +58,19 @@ public class MAHomeSubFragment2 extends BaseFragment {
         recyclerView.setOnMultiPurposeListener(new PullToRefreshRecyclerView.SimpleMultiPurposeListener() {
             @Override
             public void onRecyclerViewRefresh() {
+                LogUtil.d("1 get photo list...");
                 getPhotoList(true);
             }
 
             @Override
             public void onRecyclerViewLoadMore() {
+                LogUtil.d("2 get photo list...");
                 getPhotoList(false);
             }
 
             @Override
             public void onLoadMoreRetry() {
+                LogUtil.d("3 get photo list...");
                 getPhotoList(false);
             }
         });
@@ -88,19 +95,24 @@ public class MAHomeSubFragment2 extends BaseFragment {
     }
 
     private void getPhotoList(final boolean pullRefresh) {
+        Map<String, String> params = new HashMap<>();
+        params.put("page", String.valueOf(new Random().nextInt(100)));
+        params.put("limit", "20");
+
         new MAPhotoListExecutor()
+                .params(params)
                 .callback(new BaseExecutor.JsonCallback<List<MAHomeGoodsBean>>() {
                     @Override
                     public void onSuccess(List<MAHomeGoodsBean> response) {
                         if (pullRefresh) {
-                            recyclerView.refreshSuccess();
                             List<MAHomeBaseBean> list = new ArrayList<>();
                             list.add(new MAHomeBannerBean(1));
                             list.addAll(new ArrayList<>(response));
                             adapter.setNewData(list);
+                            recyclerView.refreshSuccess();
                         } else {
-                            recyclerView.loadMoreSuccess();
                             adapter.setLoadMoreData(new ArrayList<MAHomeBaseBean>(response));
+                            recyclerView.loadMoreSuccess();
                         }
                     }
 
