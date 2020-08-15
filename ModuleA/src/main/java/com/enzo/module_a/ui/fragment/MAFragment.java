00e73b8e42995170b5f40ea74a72979b7da19cb8
xiaofy <xiaofangyinwork@163.com>
2020-08-15 09:09:37 +0800

    update mine page

--- ModuleA/src/main/java/com/enzo/module_a/ui/fragment/MAFragment.java
+++ ModuleA/src/main/java/com/enzo/module_a/ui/fragment/MAFragment.java
@@ -67,7 +67,7 @@ public class MAFragment extends BaseFragment {
 
     @Override
     public int getLayoutId() {
-        return R.layout.ma_fragment_a2;
+        return R.layout.ma_fragment_a;
     }
 
     @Override
