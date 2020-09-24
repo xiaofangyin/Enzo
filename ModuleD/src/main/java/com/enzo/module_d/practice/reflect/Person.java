e93dbd321885cd688395adc5bdc183efff583c4b
xiaofy <fangyin.xiao@igg.com>
2020-09-24 09:35:53 +0800

    反射

---
+++ ModuleD/src/main/java/com/enzo/module_d/practice/reflect/Person.java
@@ -0,0 +1,23 @@
+package com.enzo.module_d.practice.reflect;
+
+public class Person {
+
+    int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
