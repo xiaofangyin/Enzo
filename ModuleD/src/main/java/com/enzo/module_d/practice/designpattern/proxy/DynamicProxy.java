55be17a6c2288b993b391af9dd7ae09e2c3b0df2
xiaofy <fangyin.xiao@igg.com>
2020-09-24 16:45:00 +0800

    动态代理

--- ModuleD/src/main/java/com/enzo/module_d/practice/designpattern/proxy/DynamicProxy.java
+++ ModuleD/src/main/java/com/enzo/module_d/practice/designpattern/proxy/DynamicProxy.java
@@ -14,10 +14,7 @@ public class DynamicProxy {
 
     public static void main(String[] args) {
         //    我们要代理的真实对象
-        Subject realSubject = new RealSubject();
-
-        //    我们要代理哪个真实对象，就将该对象传进去，最后是通过该真实对象来调用其方法的
-        InvocationHandler handler = new DynamicProxy_(realSubject);
+        final Subject realSubject = new RealSubject();
 
         /*
          * 通过Proxy的newProxyInstance方法来创建我们的代理对象，我们来看看其三个参数
@@ -26,36 +23,32 @@ public class DynamicProxy {
          * 第三个参数handler， 我们这里将这个代理对象关联到了上方的 InvocationHandler 这个对象上
          */
         Subject subject = (Subject) Proxy.newProxyInstance(
-                handler.getClass().getClassLoader(),
+                realSubject.getClass().getClassLoader(),
                 realSubject.getClass().getInterfaces(),
-                handler);
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("Method:" + method);
+
+                        if (method.getName().equals("rent")) {
+                            //　　在代理真实对象前我们可以添加一些自己的操作
+                            System.out.println("before rent house");
+                            //    当代理对象调用真实对象的方法时，其会自动的跳转到代理对象关联的handler对象的invoke方法来进行调用
+                            method.invoke(realSubject, args);
+                            //　　在代理真实对象后我们也可以添加一些自己的操作
+                            System.out.println("after rent house");
+                        } else if (method.getName().equals("hello")) {
+                            System.out.println("before hello");
+                            method.invoke(realSubject, args);
+                            System.out.println("after hello");
+                        }
+                        return null;
+                    }
+                });
 
         System.out.println(subject.getClass().getName());
         subject.rent();
         subject.hello("world");
-    }
-
-    public static class DynamicProxy_ implements InvocationHandler {
-        //　这个就是我们要代理的真实对象
-        private Object subject;
-
-        //    构造方法，给我们要代理的真实对象赋初值
-        public DynamicProxy_(Object subject) {
-            this.subject = subject;
-        }
-
-        @Override
-        public Object invoke(Object object, Method method, Object[] args)
-                throws Throwable {
-            //　　在代理真实对象前我们可以添加一些自己的操作
-            System.out.println("before rent house");
-            System.out.println("Method:" + method);
-            //    当代理对象调用真实对象的方法时，其会自动的跳转到代理对象关联的handler对象的invoke方法来进行调用
-            method.invoke(subject, args);
-            //　　在代理真实对象后我们也可以添加一些自己的操作
-            System.out.println("after rent house");
-            return null;
-        }
     }
 
     public static class RealSubject implements Subject {
