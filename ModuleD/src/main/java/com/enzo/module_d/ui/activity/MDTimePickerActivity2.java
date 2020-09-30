19742709bf8b5e0021caa789154713c34206fd93
xiaofy <fangyin.xiao@igg.com>
2020-09-30 14:48:52 +0800

    添加 time picker

--- ModuleD/src/main/java/com/enzo/module_d/ui/activity/MDTimePickerActivity2.java
+++ ModuleD/src/main/java/com/enzo/module_d/ui/activity/MDTimePickerActivity2.java
@@ -6,13 +6,20 @@ import android.view.View;
 import com.enzo.commonlib.base.BaseActivity;
 import com.enzo.commonlib.utils.common.LogUtil;
 import com.enzo.commonlib.widget.headerview.HeadWidget;
+import com.enzo.commonlib.widget.picker.picker.DatePickerView;
 import com.enzo.commonlib.widget.picker.picker.TimePickerView;
+import com.enzo.commonlib.widget.picker.picker.listener.OnDateSelectedListener;
 import com.enzo.commonlib.widget.picker.picker.listener.OnTimeSelectedListener;
 import com.enzo.module_d.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Date;
 
 public class MDTimePickerActivity2 extends BaseActivity {
 
     private TimePickerView timePickerView;
+    private DatePickerView datePickerView;
 
     @Override
     public int getLayoutId() {
@@ -35,6 +42,7 @@ public class MDTimePickerActivity2 extends BaseActivity {
     @Override
     public void initView() {
         timePickerView = findViewById(R.id.time_picker_24);
+        datePickerView = findViewById(R.id.date_picker_5);
     }
 
     @Override
@@ -48,6 +56,12 @@ public class MDTimePickerActivity2 extends BaseActivity {
             @Override
             public void onTimeSelected(boolean is24Hour, int hour, int minute, int second, boolean isAm) {
                 LogUtil.d("hour: " + hour + "...minute: " + minute + "...second: " + second + "...isAm: " + isAm);
+            }
+        });
+        datePickerView.setOnDateSelectedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(int year, int month, int day, @NotNull Date date) {
+                LogUtil.d("year: " + year + "...month: " + month + "...day: " + day);
             }
         });
     }
