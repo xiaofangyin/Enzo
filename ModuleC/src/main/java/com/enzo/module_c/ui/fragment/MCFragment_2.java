850aa8dea7c0c2e6cf0ce40e3924c3e7d5faf07f
xiaofy <fangyin.xiao@igg.com>
2020-10-09 14:34:55 +0800

    添加 AllAngleExpandableButton

--- ModuleC/src/main/java/com/enzo/module_c/ui/fragment/MCFragment_2.java
+++ ModuleC/src/main/java/com/enzo/module_c/ui/fragment/MCFragment_2.java
@@ -2,11 +2,17 @@ package com.enzo.module_c.ui.fragment;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.enzo.commonlib.base.BaseFragment;
+import com.enzo.commonlib.utils.toast.ToastUtil;
+import com.enzo.commonlib.widget.allangleexpandablebutton.AllAngleExpandableButton;
+import com.enzo.commonlib.widget.allangleexpandablebutton.ButtonData;
+import com.enzo.commonlib.widget.allangleexpandablebutton.ButtonEventListener;
 import com.enzo.module_c.R;
 import com.enzo.module_c.model.ColumnBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 文 件 名: MCFragment_2
@@ -16,8 +22,6 @@ import com.enzo.module_c.model.ColumnBean;
  */
 public class MCFragment_2 extends BaseFragment {
 
-    private TextView textView;
-
     @Override
     public int getLayoutId() {
         return R.layout.mc_fragment_c_child_2;
@@ -25,7 +29,10 @@ public class MCFragment_2 extends BaseFragment {
 
     @Override
     public void initView(View rootView) {
-        textView = rootView.findViewById(R.id.text_view);
+        installButton90to90(rootView);
+        installButton90to180(rootView);
+        installButton110to250(rootView);
+        installButton0to360(rootView);
     }
 
     @Override
@@ -38,9 +45,105 @@ public class MCFragment_2 extends BaseFragment {
         Bundle bundle = getArguments();
         if (bundle != null) {
             ColumnBean bean = (ColumnBean) getArguments().getSerializable("entity");
-            if (bean != null) {
-                textView.setText(bean.getColumn_name());
-            }
         }
+    }
+
+    private void installButton90to90(View rootView) {
+        final AllAngleExpandableButton button = (AllAngleExpandableButton) rootView.findViewById(R.id.button_expandable_90_90);
+        final List<ButtonData> buttonDatas = new ArrayList<>();
+        int[] drawable = {R.drawable.plus, R.drawable.mark, R.drawable.settings, R.drawable.heart};
+        int[] color = {R.color.mc_blue, R.color.mc_red, R.color.mc_green, R.color.mc_yellow};
+        for (int i = 0; i < 4; i++) {
+            ButtonData buttonData;
+            if (i == 0) {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 15);
+            } else {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 0);
+            }
+            buttonData.setBackgroundColorId(rootView.getContext(), color[i]);
+            buttonDatas.add(buttonData);
+        }
+        button.setButtonDatas(buttonDatas);
+        setListener(button);
+    }
+
+    private void installButton90to180(View rootView) {
+        final AllAngleExpandableButton button = (AllAngleExpandableButton) rootView.findViewById(R.id.button_expandable_90_180);
+        final List<ButtonData> buttonDatas = new ArrayList<>();
+        int[] drawable = {R.drawable.plus, R.drawable.mark, R.drawable.settings, R.drawable.heart};
+        int[] color = {R.color.mc_blue, R.color.mc_red, R.color.mc_green, R.color.mc_yellow};
+        for (int i = 0; i < 4; i++) {
+            ButtonData buttonData;
+            if (i == 0) {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 15);
+            } else {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 0);
+            }
+            buttonData.setBackgroundColorId(rootView.getContext(), color[i]);
+            buttonDatas.add(buttonData);
+        }
+        button.setButtonDatas(buttonDatas);
+        setListener(button);
+    }
+
+    private void installButton110to250(View rootView) {
+        final AllAngleExpandableButton button = (AllAngleExpandableButton) rootView.findViewById(R.id.button_expandable_110_250);
+        final List<ButtonData> buttonDatas = new ArrayList<>();
+        int[] drawable = {R.drawable.edit, R.drawable.refresh, R.drawable.mark, R.drawable.settings, R.drawable.heart, R.drawable.search, R.drawable.copy};
+        int[] color = {R.color.mc_black, R.color.mc_orange, R.color.mc_blue, R.color.mc_pink, R.color.mc_yellow, R.color.mc_red, R.color.mc_green};
+        for (int i = 0; i < 7; i++) {
+            ButtonData buttonData;
+            if (i == 0) {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 15);
+            } else {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 0);
+            }
+            buttonData.setBackgroundColorId(rootView.getContext(), color[i]);
+            buttonDatas.add(buttonData);
+        }
+        button.setButtonDatas(buttonDatas);
+        setListener(button);
+    }
+
+    private void installButton0to360(View rootView) {
+        final AllAngleExpandableButton button = (AllAngleExpandableButton) rootView.findViewById(R.id.button_expandable_0_360);
+        final List<ButtonData> buttonDatas = new ArrayList<>();
+        int[] drawable = {R.drawable.edit, R.drawable.refresh, R.drawable.mark, R.drawable.settings, R.drawable.heart, R.drawable.search, R.drawable.copy};
+        int[] color = {R.color.mc_black, R.color.mc_orange, R.color.mc_blue, R.color.mc_pink, R.color.mc_yellow, R.color.mc_red, R.color.mc_green};
+        for (int i = 0; i < 7; i++) {
+            ButtonData buttonData;
+            if (i == 0) {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 15);
+            } else {
+                buttonData = ButtonData.buildIconButton(rootView.getContext(), drawable[i], 0);
+            }
+            buttonData.setBackgroundColorId(rootView.getContext(), color[i]);
+            buttonDatas.add(buttonData);
+        }
+        button.setButtonDatas(buttonDatas);
+        setListener(button);
+    }
+
+    private void setListener(AllAngleExpandableButton button) {
+        button.setButtonEventListener(new ButtonEventListener() {
+            @Override
+            public void onButtonClicked(int index) {
+                showToast("clicked index:" + index);
+            }
+
+            @Override
+            public void onExpand() {
+//                showToast("onExpand");
+            }
+
+            @Override
+            public void onCollapse() {
+//                showToast("onCollapse");
+            }
+        });
+    }
+
+    private void showToast(String toast) {
+        ToastUtil.show(toast);
     }
 }
