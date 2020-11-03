079e4683453868a965f253656c6b85de864d6f68
xiaofy <fangyin.xiao@igg.com>
2020-11-03 11:39:51 +0800

    crash

--- Enzo/BaseLib/CommonLib/src/main/java/com/enzo/commonlib/utils/crashlib/ui/ExceptionCaughtAdapter.java
+++ Enzo/BaseLib/CommonLib/src/main/java/com/enzo/commonlib/utils/crashlib/ui/ExceptionCaughtAdapter.java
@@ -2,6 +2,8 @@ package com.enzo.commonlib.utils.crashlib.ui;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+
 import com.enzo.commonlib.utils.common.ApkUtils;
 import com.enzo.commonlib.utils.crashlib.util.CrashHelper;
 
@@ -9,8 +11,8 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 public class ExceptionCaughtAdapter implements UncaughtExceptionHandler {
 
-    private Application application;
-    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+    private final Application application;
+    private final Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
 
     public ExceptionCaughtAdapter(Application application, Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
         this.application = application;
@@ -18,7 +20,7 @@ public class ExceptionCaughtAdapter implements UncaughtExceptionHandler {
     }
 
     @Override
-    public void uncaughtException(Thread thread, Throwable ex) {
+    public void uncaughtException(@NonNull Thread thread, @NonNull Throwable ex) {
         String crashInfo = CrashHelper.buildCrashInfo(ex);
         CrashHelper.saveCrashLogToLocal(application, crashInfo);
         if (ApkUtils.isAppDebug(application)) {//debug模式下才显性的展示崩溃信息
