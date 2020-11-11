a5d3b95a8d37528629f26a4d1c13e792b6160014
xiaofy <fangyin.xiao@igg.com>
2020-11-11 10:00:19 +0800

    添加广告页面

--- Enzo/Module/Main/src/main/java/com/enzo/main/ui/activity/SAAdvertActivity.java
+++ Enzo/Module/Main/src/main/java/com/enzo/main/ui/activity/SAAdvertActivity.java
@@ -4,51 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.KeyEvent;
+import android.widget.ImageView;
 
 import com.enzo.commonlib.base.BaseActivity;
+import com.enzo.commonlib.utils.imageloader.ImageLoader;
 import com.enzo.commonlib.utils.statusbar.bar.StateAppBar;
-import com.enzo.commonlib.utils.statusbar.utils.StatusBarUtils;
+import com.enzo.main.R;
 
-/**
- * 文 件 名: SASplashActivity
- * 创 建 人: xiaofangyin
- * 创建日期: 2017/12/23
- * 邮   箱: xiaofangyinwork@163.com
- */
-public class SASplashActivity extends BaseActivity {
+public class SAAdvertActivity extends BaseActivity {
 
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.main_activity_advert;
     }
 
     @Override
     public void initView() {
-        //隐藏信号栏
         StateAppBar.translucentStatusBar(this, true);
-        StatusBarUtils.StatusBarLightMode(this);
     }
 
     @Override
     public void initData(Bundle savedInstanceState) {
+        ImageView ivAd = findViewById(R.id.iv_advert);
+        new ImageLoader.Builder(this)
+                .load(R.mipmap.ic_launcher)
+                .build()
+                .into(ivAd);
+
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), SAAdvertActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SAMainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 3000);
     }
 
     @Override
     public void initListener() {
 
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return keyCode == KeyEvent.KEYCODE_BACK;
     }
 }
